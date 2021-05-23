132c0ef23d17d9d2feb53876f8109fe442d90cea
Ajay <ajay.23432@gmail.com>
2021-05-23 07:27:12 +0100

    ffsf

---
+++ file naming.py
@@ -0,0 +1,16 @@
+
+
+
+import requests
+from tkinter import *
+
+from tkinter import messagebox
+from datetime import datetime
+
+root = Tk()
+root.geometry('900x1100')
+root.title("Covid Cases")
+root.configure(bg = '#49A')
+
+
+root.mainloop()
